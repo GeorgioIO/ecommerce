31f26f5807aee05079f2f1da2468be6bbff7c0c7
GeorgioJabbour <georgiojabbour.g.gj@gmail.com>
2026-01-04 15:45:10 +0200

    Add javaScript api backend functions

--- frontend/js/genres/genreServices.js
+++ frontend/js/genres/genreServices.js
@@ -3,3 +3,61 @@ export async function fetch_genres_DB() {
 
   return result.json();
 }
+
+// Function responsible to add a genre
+export async function addGenre_DB(genreData) {
+  const formData = new FormData();
+  formData.append("name", genreData.name);
+  formData.append("image", genreData.image);
+
+  const result = await fetch("../backend/genres/add_genre.php", {
+    method: "POST",
+    body: formData,
+  });
+
+  return result.json();
+}
+
+// Function responsible for updating a genre
+export async function updateGenre_DB(genreData) {
+  const formData = new FormData();
+
+  formData.append("id", genreData.id);
+  formData.append("name", genreData.name);
+  formData.append("image", genreData.image);
+
+  const result = await fetch("../backend/genres/update_genre.php", {
+    method: "POST",
+    body: formData,
+  });
+
+  return result.json();
+}
+
+export async function deleteGenre_DB(genreID) {
+  const result = await fetch("../backend/genres/delete_genre.php", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams({
+      id: genreID,
+    }),
+  });
+
+  return result.json();
+}
+
+export async function getGenreData_DB(genreID) {
+  const result = await fetch("../backend/genres/fetch_single_genre.php", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams({
+      id: genreID,
+    }),
+  });
+
+  return result.json();
+}
