cc84cc8f811e160a3b57ae0252cd076440ac4fb1
GeorgioJabbour <georgiojabbour.g.gj@gmail.com>
2026-01-07 15:20:57 +0200

    Create fetch single customer api

---
+++ backend/customers/fetch_single_customer.php
@@ -0,0 +1,66 @@
+<?php
+
+header('Content-Type: application/json');
+require_once  __DIR__ . '/../../config/database.php';
+require_once  __DIR__ . '/../validators/customer_validators.php';
+
+
+$id = $_POST["id"] ?? null;
+
+// validate author id
+$customer_id_result = validate_customer_id($id);
+if(!$customer_id_result['success'])
+{
+    echo json_encode([
+        'success' => false,
+        'message' => $customer_id_result['message']
+    ]);
+    exit;
+}
+
+$DB_customer_id = $customer_id_result['value'];
+
+$query = <<<EOT
+
+SELECT
+    u.id,
+    u.customer_code,
+    u.name,
+    u.email,
+    u.phone_number,
+    u.date_added,
+    COALESCE(SUM(o.total_price), 0) AS total_spent,
+    COUNT(o.id) AS total_orders
+FROM users u
+LEFT JOIN orders o ON u.id = o.user_id
+WHERE u.id = ?
+GROUP BY u.id
+EOT;
+
+$stmt = $conn->prepare($query);
+$stmt->bind_param("i" , $DB_customer_id);
+$stmt->execute();
+
+$result = $stmt->get_result();
+
+// id isnt found in database
+if($result->num_rows === 0){
+    echo json_encode([
+        'success' => false, 
+        'data' => 'Customer not found'
+    ]);
+    exit;
+}
+
+$author = $result->fetch_assoc();
+
+$stmt->close();
+$conn->close();
+
+echo json_encode([
+    'success' => true,
+    'data' => $author
+]);
+exit;
+
+?>
---
+++ backend/validators/customer_validators.php
@@ -0,0 +1,41 @@
+<?php
+
+
+require_once  __DIR__ .  '/../../config/database.php';
+require_once  __DIR__ . '/../../config/helpers.php';
+
+function validate_customer_id($id)
+{
+    if($id === null || empty($id) || empty(trim($id)))
+    {
+        return [
+            'success' => false,
+            'message' => 'Invalid ID'
+        ];
+    }
+
+    $id = trim($id);
+
+    if(!ctype_digit($id))
+    {
+        return [
+            'success' => false,
+            'message' => 'Invalid Customer ID must be a positive integer'
+        ];
+    }
+
+    if((int) $id <= 0)
+    {
+        return [
+            'success' => false,
+            'message' => 'Invalid Customer ID must be higher than 0'
+        ];
+    }
+
+    return [
+        'success' => true,
+        'value' => (int) $id
+    ];
+}
+
+?>
