e103e50a174b8fb9b39bc622a2a8923340a28f3c
GeorgioJabbour <georgiojabbour.g.gj@gmail.com>
2026-02-05 12:00:05 +0200

    Refactor CSS Structure
    - Split styles.css into four files base/layout/components/responsive
    - this is to improve maintanability in the future as the project grows
    - No visual changes spotted
    
    Base :
    - contains resets, font faces, variables, general styles
    
    Layout :
    - layout contains things that exist on every single page ex : header ,
      sidebar, footer...
    
    Components :
    - Contains content blocks like product-card , community, reviews...
    
    Responsive :
    - Contains media queries

--- frontend/storefront/pages/home.php
+++ frontend/storefront/pages/home.php
@@ -4,7 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BookNest</title>
-    <link rel="stylesheet" href="../css/styles.css">
+    <link rel="stylesheet" href="../css/base.css">
+    <link rel="stylesheet" href="../css/layout.css">
+    <link rel="stylesheet" href="../css/component.css">
+    <link rel="stylesheet" href="../css/responsive.css">
     <script defer type="module" src="../js/main.js"></script>
 </head>
 <body>
--- frontend/storefront/pages/my-account.php
+++ frontend/storefront/pages/my-account.php
@@ -10,7 +10,10 @@ include __DIR__ . '../../../../configuration/session.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Account - BookNest</title>
-    <link rel="stylesheet" href="../css/styles.css">
+    <link rel="stylesheet" href="../css/base.css">
+    <link rel="stylesheet" href="../css/layout.css">
+    <link rel="stylesheet" href="../css/component.css">
+    <link rel="stylesheet" href="../css/responsive.css">
     <script defer type="module" src="../js/main.js"></script>
 </head>
 <body>
