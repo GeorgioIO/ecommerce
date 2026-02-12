307984becb648de2e38e2d31b420824530c8b661
GeorgioJabbour <georgiojabbour.g.gj@gmail.com>
2026-02-12 13:24:42 +0200

    test

--- frontend/storefront/includes/account-dashboard.php
+++ frontend/storefront/includes/account-dashboard.php
@@ -5,5 +5,28 @@ require_once __DIR__ . '../../../../configuration/session.php';
 ?>
 
 <main>
-    <section id="user-dashboard"></section>
+    <section id="user-dashboard-section">
+        <div id="user-dashboard">
+                <ul id="user-dashboard-sidebar">
+                    <li>
+                        <button>
+                            Dashboard
+                        </button>
+                    </li>
+                    <li>
+                        <button>Orders</button>
+                    </li>
+                    <li>
+                        <button>Addresses</button>
+                    </li>
+                    <li>
+                        <button>Account details</button>
+                    </li>
+                    <li>
+                        <button>Log out</button>
+                    </li>
+                </ul>
+                <div id="user-dashboard-content">p</div>
+        </div>
+    </section>
 </main>
