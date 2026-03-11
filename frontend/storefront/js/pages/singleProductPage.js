b1c479ea86d378c5dd22831a0591e28ed8068d24
GeorgioJabbour <georgiojabbour.g.gj@gmail.com>
2026-03-11 17:21:07 +0200

    Work on buy now button

--- frontend/storefront/js/pages/singleProductPage.js
+++ frontend/storefront/js/pages/singleProductPage.js
@@ -1,9 +1,29 @@
 import { updateCardPrice } from "../components/cartCard.js";
+import {
+  activateMessageBox,
+  appendMessageBox,
+  createMesssageBox,
+} from "../components/messageBox.js";
 import { calculateCartTotal, updateCart } from "../components/miniCartBar.js";
 import { handleCartButton } from "../components/productCard.js";
 
 document.addEventListener("click", async (e) => {
   const addToCartButton = e.target.closest("#single-product-adc-button");
+  const buyNowButton = e.target.closest("#buy-now-button");
+
+  if (buyNowButton) {
+    const product = e.target.closest("#product");
+    const response = await handleCartButton(product, buyNowButton);
+
+    if (response.success) {
+      window.location.href = "../pages/checkout.php";
+    } else {
+      activateMessageBox();
+      const messageBox = createMesssageBox(response.message);
+      appendMessageBox(messageBox);
+      return;
+    }
+  }
 
   if (addToCartButton) {
     const product = e.target.closest("#product");
